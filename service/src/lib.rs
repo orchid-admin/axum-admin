d7a2bdb9bc9019f97c5d3abfacdb366c64a85c45
tingfeng-key <1770963469@qq.com>
2023-04-06 23:55:35 +0800

    尝试service特征

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod prisma;
+
+use prisma_client_rust::WhereQuery;
+#[derive(Debug)]
+struct User<'a> {
+    username: &'a str,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+impl<'a> WhereQuery<'a> for User<'a> {}
